c8b995c0a24aa36354e196e5e7662b737070d9a4
Chris <chrisbracewell35@gmail.com>
2021-12-12 21:17:53 -0500

    adding highscore feature

--- assets/js/game.js
+++ assets/js/game.js
@@ -185,6 +185,7 @@ var fight = function(enemy) {
 
 //Start game function
 var startGame = function () {
+  debugger;
         // reset player stats
   playerInfo.reset ();
   // other logic remains the same...
@@ -227,11 +228,22 @@ for (var i = 0; i < enemyInfo.length; i++) {
 var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
-  // if player is still alive, player wins!
-  if (playerInfo.health > 0) {
-    window.alert("Great job, you've survived the game! You now have a score of " + playerInfo.money + '.');
-  } else {
-    window.alert("You've lost your robot in battle!");
+  // CHECK STOREAGE FOR HIGHSCORE//
+var highScore = localStorage.getItem("highscore");
+
+if (highScore === null)
+ {   highScore = 0;
+ }
+
+// player beeat highscore//
+ if ( playerInfo.money > highScore) {
+localStorage.setItem(" highscore", playerInfo.money);
+localStorage.setItem("name", playerInfo.name);
+
+   alert(playerInfo.name + "now has the high score of " + playerInfo.money + "!");
+  } 
+  else {
+    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
   }
 
   // ask player if they'd like to play again
